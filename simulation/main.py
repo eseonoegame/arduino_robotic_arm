5c3b738a93eb27a13707276878e97e05a25a8b03
Noe Game <noe.game@reseau.eseo.fr>
2024-12-24 17:22:25 +0100

    branche objet

--- simulation/main.py
+++ simulation/main.py
@@ -8,256 +8,261 @@ import console as console
 import matplotlib.pyplot as plt
 from math import *
 
+# --- Classes ---
+class Target :
+    def __init__(self,x,y,t):
+        self.x = x
+        self.y = y
+        self.t = t 
+
+    def __str__(self):
+        return (f"Position de l'objet : ({self.x},{self.y})"
+                "\nAngle d'approche : {self.t}")
+
+    def show(self):
+        """Dessine l'objet à attraper."""
+        plt.plot(self.x, self.y, 'ro')
+        plt.pause(0.05)
+        plt.show()
+
+class RobotArm :
+    def __init__(self,A,B):
+        """ 
+        Angle par défaut du bras robot. 
+        Longueur des segments du bras robot.
+        """
+        self.A = A
+        self.B = B
+
+        self.xRangeMax = sum(B)-B[0]
+        self.xRangeMin = 0
+        self.yRangeMax = sum(B)-B[0]
+        self.yRangeMin = 0
+
+        self.A = degreeToRadian(A)
+        self.Sx = [0, 0, 0, 0, 0]
+        self.Sy = [0, 0, 0, 0, 0]
+
+    def __str__(self):
+        return (f"Longueur des segments : {self.B}"
+                "\nAngles des servomoteurs : {self.A}")
+
+    def showAngles(self):
+        """ Affiche les angles du bras robot. """
+        liste = []
+        largeur = 15
+
+        for i in range(len(self.A)):
+            liste.append([f"Angle {i} : ",f"{round(degrees(self.A[i]),3)} deg", f"{round(self.A[i],3)} rad"])
+        
+        print("\n" + largeur*3*"-")
+        console.tabDisplay(liste, largeur)
+        print(largeur*3*"-"+ "\n")
+
+    def show(self):
+        """Dessine le bras robotique."""
+        Sx,Sy = self.calculCoordonne()
+        for i in range(len(Sx)-1):
+            plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
+        plt.pause(0.05)
+        plt.draw()
+
+    def calculCoordonne(self):
+        """ 
+        Calcul les coordonnées des points du bras robot.
+        Paramètres : Les angles et les longueurs des segments du bras.
+        Retourne : Les coordonnées des points des segments du bras.
+        """
+
+        A = self.A
+        B = self.B
+        Sx = self.Sx
+        Sy = self.Sy
+
+        Sx,Sy = [],[]
+        for i in range(len(A)+1):
+            Sx.append(0)
+            Sy.append(0)
+
+        Sx[0] = 0
+        Sy[0] = 0
+
+        Sx[1] = Sx[0] + B[0]*sin(A[0])
+        Sy[1] = Sy[0] + B[0]*cos(A[0])
+
+        Sx[2] = Sx[1] + B[1]*sin(A[0]+A[1])
+        Sy[2] = Sy[1] + B[1]*cos(A[0]+A[1])
+
+        Sx[3] = Sx[2] + B[2]*sin(A[0]+A[1]+A[2])
+        Sy[3] = Sy[2] + B[2]*cos(A[0]+A[1]+A[2])
+
+        Sx[4] = Sx[3] + B[3]*sin(A[0]+A[1]+A[2]+A[3])
+        Sy[4] = Sy[3] + B[3]*cos(A[0]+A[1]+A[2]+A[3])
+
+        return Sx, Sy
+
+    def calculAngle(self, target):
+        """ 
+        Calcule les angles du bras robot pour atteindre un point (x,y) avec une pince dans un angle t. 
+        """
+
+        B = self.B
+        xTarget = target.x
+        yTarget = target.y
+        t = target.t
+
+        A = []
+        for i in range(len(B)):
+            A.append(0)
+
+        A[0] = 0 
+
+        num = xTarget**2 + (yTarget-B[0])**2 + B[3]**2 - 2*B[3] * (xTarget*sin(t) + (yTarget-B[0])*cos(t)) - B[1]**2 - B[2]**2  
+        den = 2*B[1]*B[2]
+        r = num/den
+
+        A[2] = acos(r)
+
+        K = B[1] + B[2]*r 
+        num = K*(-B[0] + yTarget - B[3]*cos(t)) + B[2]*sin(A[2])*(xTarget-B[3]*sin(t)) 
+        den = K**2 + (B[2]**2)*(1-cos(A[2])**2)  
+        r = num/den
+
+        A[1] = acos(r)
+
+        A[3] = t - A[0] - A[1] - A[2]
+
+        self.A = A
+        return A
+
+    def moovePinceToCoordonate(self,start,end,animation = False):
+        """
+        Approche le bras robotique vers un objet.
+        Paramètres :
+        - target : les coordonnées de l'objet.
+        - animation : True pour afficher l'animation.
+        """
+        
+        # Start and end position
+        xStart = start[0]
+        yStart = start[1]
+        xEnd = end[0]
+        yEnd = end[1]
+
+        while xStart != xEnd and yStart != yEnd:
+
+            if xStart > xEnd:
+                while xStart > xEnd:
+                    xStart -= 1
+
+            elif xStart < xEnd:
+                while xStart < xEnd:
+                    xStart += 1
+
+            if yStart > yEnd:
+                while yStart > yEnd:
+                    yStart -= 1
+
+            elif yStart < yEnd:
+                while yStart < yEnd:
+                    yStart += 1
+  
+            target = Target(xStart,yStart,0)
+            A = self.calculAngle(target)
+            self.goToPosition(A,animation)
+    
+
+    def goToPosition(self,A,animation = False):
+        """
+        Met le bras dans la position décrite par la liste d'angles A.
+        """
+        self.A = A
+        if animation == True:
+            self.show()
+
+# --- Fonctions ---
+
 def degreeToRadian(A):
     """ Convertit les angles en degrés en radians. """
     for i in range(len(A)):
         A[i] = radians(A[i])
     return A
 
-
-def calculCoordonne(B, A):
-    """ Calcule les coordonnées des points du bras robot. """
-
-    Sx,Sy = [],[]
-    for i in range(len(A)+1):
-        Sx.append(0)
-        Sy.append(0)
-
-    Sx[0] = 0
-    Sy[0] = 0
+def demo1(bras,target):
     
-    Sx[1] = Sx[0] + B[0]*sin(A[0])
-    Sy[1] = Sy[0] + B[0]*cos(A[0])
-
-    Sx[2] = Sx[1] + B[1]*sin(A[0]+A[1])
-    Sy[2] = Sy[1] + B[1]*cos(A[0]+A[1])
-
-    Sx[3] = Sx[2] + B[2]*sin(A[0]+A[1]+A[2])
-    Sy[3] = Sy[2] + B[2]*cos(A[0]+A[1]+A[2])
-
-    Sx[4] = Sx[3] + B[3]*sin(A[0]+A[1]+A[2]+A[3])
-    Sy[4] = Sy[3] + B[3]*cos(A[0]+A[1]+A[2]+A[3])
-
-    return Sx, Sy
-
-
-def calculAngle(B, x, y, t):
-    """ Calcule les angles du bras robot. """
+    bras.calulAngle(target)
+    bras.goToPosition(bras.A,True)
     
-    A = []
-    for i in range(len(B)):
-        A.append(0)
+    target.x = bras.Sx[-1]
 
-    A[0] = 0 
+    while target.x < bras.xRangeMax:
+        target.x += 1
+        bras.calculAngle(target)
+        bras.goToPosition(bras.A,True)
+
+    while target.x > bras.xRangeMin:
+        target.x -= 1
+        bras.calculAngle(target)
+        bras.goToPosition(bras.A,True)
+
+def simulateur():
     
-    num = x**2 + (y-B[0])**2 + B[3]**2 - 2*B[3] * (x*sin(t) + (y-B[0])*cos(t)) - B[1]**2 - B[2]**2  
-    den = 2*B[1]*B[2]
-    r = num/den
+    # --- Paramètres objets ---
     
-    A[2] = acos(r)
-    
-    K = B[1] + B[2]*r 
-    num = K*(-B[0] + y - B[3]*cos(t)) + B[2]*sin(A[2])*(x-B[3]*sin(t)) 
-    den = K**2 + (B[2]**2)*(1-cos(A[2])**2)  
-    r = num/den
-
-    A[1] = acos(r)
-    
-    A[3] = t - A[0] - A[1] - A[2]
-    
-    return A
-
-
-def AffichageAngles(A):
-    """ Affiche les angles du bras robot. """
-    liste = []
-    largeur = 15
-
-    for i in range(len(A)):
-        liste.append([f"Angle {i} : ",f"{round(degrees(A[i]),3)} deg", f"{round(A[i],3)} rad"])
-    
-    print("\n" + largeur*3*"-")
-    console.tabDisplay(liste, largeur)
-    print(largeur*3*"-"+ "\n")
-
-
-def affichageBras(Sx, Sy,xmin,ymin,xmax,ymax):
-    """ Affiche les segments du bras robot. """
-    plt.clf()
-    plt.grid(True)
-    plt.xlim(xmin, xmax)
-    plt.ylim(ymin, ymax)
-    for i in range(len(Sx)-1):
-        plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
-    plt.pause(0.05)
-    plt.show()
- 
-
-def demonstration1(Sx, Sy, x, B, A, xmin, ymin, xmax, ymax, y, t, xMax, xMin):
-    """ Animation du bras. """
-
-    while x < xMax:
-        x += 1
-        A = calculAngle(B, x, y, t)  # Calculate new angles
-        Sx, Sy = calculCoordonne(B, A)
-        plt.clf()
-        plt.grid(True)
-        plt.axis('equal')
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        for i in range(len(Sx)-1):
-            plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
-        plt.draw()
-        plt.pause(0.05)
-        
-    while x > xMin:
-        x -= 1
-        A = calculAngle(B, x, y, t)  # Calculate new angles
-        Sx, Sy = calculCoordonne(B, A)
-        plt.clf()
-        plt.grid(True)
-        plt.axis('equal')
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        for i in range(len(Sx)-1):
-            plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
-        plt.draw()
-        plt.pause(0.05)
-
-def GoTakeObject(Sx, Sy, x, B, A, AxeXMin, AxeYMin, AxeXMax, AxeYMax, y, t, xMax, xMin):
-    """Approche du bras robotique vers l'objet."""
-    
-    # 1. On affiche les segments du bras robot avec les angles par défaut.
-    # 2. On demande à l'utilisateur de saisir les coordonnées de l'objet.
-    # 3. On calcule les angles pour atteindre l'objet.
-    # 4. On affiche le bras qui tend progressivement vers la position de l'objet.
-    
-    # 1. On affiche les segments du bras robot avec les angles par défaut.
-
-    Sx,Sy = calculCoordonne(B, A)
-    plt.clf()
-    plt.grid(True)
-    plt.xlim(AxeXMin, AxeXMax)
-    plt.ylim(AxeYMin, AxeYMax)
-    for i in range(len(Sx)-1):
-        plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
-    plt.pause(0.05)
-    plt.draw()
-
-    # 2. On demande à l'utilisateur de saisir les coordonnées de l'objet.
-
-    xTarget = askFor.ABoundedNumber("Distance de l'objet : ", xMin, xMax)
-    yTarget = askFor.ABoundedNumber("Hauteur de l'objet : ",0,100)
-    tTarget = askFor.ABoundedNumber("Angle d'approche : ",0,2*pi)
-
-    xPince = Sx[-1]
-    yPince = Sy[-1]
-    tPince = t
-
-    print(f"\nPosition actuelle de la pince : ({xPince},{yPince},{tPince})")
-    print(f"Position de l'objet : ({xTarget},{yTarget},{tTarget})")
-
-    # 3. On calcule les angles pour atteindre l'objet.
-    # 4. On affiche le bras qui tend progressivement vers la position de l'objet.
-
-    while xPince < xTarget:
-        x += 1
-        while yPince < yTarget:
-            y += 1
-            while tPince < tTarget:
-                t += 0.1
-                
-                A = calculAngle(B, x, y, t)
-                Sx, Sy = calculCoordonne(B, A)
-                plt.clf()
-                plt.grid(True)
-                plt.axis('equal')
-                plt.xlim(AxeXMin, AxeXMax)
-                plt.ylim(AxeXMin, AxeYMax)
-                for i in range(len(Sx)-1):
-                    plt.plot([Sx[i], Sx[i+1]], [Sy[i], Sy[i+1]], label=f"B{i}")
-                plt.draw()
-                plt.pause(0.05)
-
-
-def main():
-
-    # --- Paramètres objet ---
-    
-    xTarget = 15        # distance de l'objet
-    yTarget = 0         # hauteur de l'objet
-    tTarget = 3/4*pi    # angle d'approche
-
-    xTargetMax = 160
-    xTargetMin = 30
-    yTargetMax = 160
-    yTargetMin = 0
-
-    # --- Paramètres robot ---
-    
-    h = 60                # Hauteur du socle du bras
-    B = [h, 100, 80, 20]  # B0,B1,B2,B3 (longueur des segments en mm)
-    A = [0, 5, 175, 5]   # A0,A1,A2,A3 (angles entre les segments en degré)
+    arm = RobotArm([0, 45, 45, 45],[50, 80, 60, 20])
+    target = Target(15,0,3/4*pi)
 
     # ---- Paramètres simulateur ---
+    
+    axeXMin = - 10
+    axeYMin = - 10
+    AxeXMax = arm.xRangeMax + 10
+    axeYMax = arm.yRangeMax + 10
 
-    axeXMin = -10
-    axeYMin = -10
-    AxeXMax = xTargetMax+10
-    axeYMax = yTargetMax+10
+    # --- Initialisation de la fenêtre graphique ---
 
-    # --- Initialisation ---
+    plt.figure()
 
-    A = degreeToRadian(A)
-    Sx = [0, 0, 0, 0, 0]
-    Sy = [0, 0, 0, 0, 0]
+    # --- Choix de la simulation ---
 
-    plt.figure(figsize=(10, 8))
-    plt.clf()
-    plt.grid(True)
-    plt.axis('equal')
-    plt.xlim(axeXMin, AxeXMax)
-    plt.ylim(axeYMin, axeYMax)
-
-    # --- Simulateur ---
-      
     isEnd = False
     
     while isEnd == False:
         
-        print("\nAffichage des segments du bras robot : "
-              "\n0. pour des angles par défaut."
-              "\n1. pour un objet à une distance x donnée."
-              "\n2. pour un objet à une distance x variant entre xmin et xmax."
-              "\n3. pour aller attraper un objet."
+        print("\nAffichage des segments du bras robot : ")
+
+        print("\n0. pour des angles par défaut."
+              "\n1. pour la pince sur un objet à une distance x,y donnée."
+              "\n2. pour la pince sur un objet à une distance x variant entre xmin et xmax."
+              "\n3. pour aller attraper un objet depuis angle par défaut."
               "\n4. Quitter.")
         
         choix = askFor.ABoundedNumber("Choix : ", 0, 4)
 
-        # On affiche les segments du bras robot avec les angles par défaut.
         if choix == 0:
-            Sx,Sy = calculCoordonne(B, A)
-            AffichageAngles(A)
-            affichageBras(Sx, Sy,axeXMin,axeYMin,AxeXMax,axeYMax)
-            
-        # On affiche les segments du bras robot avec les angles calculés pour atteindre un objet à une distance x donnée.
+            arm.show()
+            arm.showAngles()
+        
         elif choix == 1:
-            A = calculAngle(B, xTarget, yTarget, tTarget)
-            Sx,Sy = calculCoordonne(B, A)
-            affichageBras(Sx, Sy, axeXMin, axeYMin, AxeXMax, axeYMax)
-            AffichageAngles(A)
-        
-        # On affiche les segments du bras robot avec les angles calculés pour atteindre un objet à une distance x variant entre xmin et xmax.
-        elif choix == 2:
-            demonstration1(Sx, Sy, xTarget, B, A, axeXMin, axeYMin, AxeXMax, axeYMax, yTarget, tTarget, xTargetMax, xTargetMin)
-        
-        # On affiche les segments du bras robot avec les angles qui varient.
-        elif choix == 3:
-            Sx,Sy = calculCoordonne(B, A)
-            GoTakeObject(Sx, Sy, xTarget, B, A, axeXMin, axeYMin, AxeXMax, axeYMax, yTarget, tTarget, xTargetMax, xTargetMin)
+            target.x = askFor.ABoundedNumber("x : ", arm.xRangeMin, arm.xRangeMax)
+            target.y = askFor.ABoundedNumber("y : ", arm.yRangeMin, arm.yRangeMax)
+            arm.calculAngle(target)
+            arm.goToPosition(arm.A)
+            #target.show()
 
-        else :
-            isEnd = True
-            print("Au revoir !")
-    
-main()
+        elif choix == 2:
+            target.y = 0
+            target.x = 30
+            demo1(arm,target)
+
+        elif choix == 3:
+            arm.goToPosition([0,0,0,0],True)
+            target.x = arm.Sx[-1]
+            target.y = arm.Sy[-1]
+            arm.moovePinceToCoordonate(target,True)
+            target.show()
+
+        elif choix == 4: 
+            isEnd = True 
+
+simulateur()
